996e63570ac4fdd9183c8af0f2b75afea607692a
Fredrick <freddyfred976@gmail.com>
2023-07-21 06:45:34 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Greetings from "./Greetings";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Greetings/>
     </div>
   );
 }
--- src/Greetings.js
+++ src/Greetings.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-function Greetins() {
-    return(
+function Greetings() {
+    return (
         <>
-        <h1>THIS IS MY OFFICIAL FIRST TRIAL!</h1>
-        <p>Hope it works.........</p>
+            <h1>THIS IS MY OFFICIAL FIRST TRIAL!</h1>
+            <p>Hope it works.........</p>
         </>
     ); 
 }
+
+export default Greetings;
